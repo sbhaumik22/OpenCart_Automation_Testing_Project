763524fa03f790e8f24f8240b6cc53f2deec81ed
Soumesh Bhaumik <soumeshbhaumik99@gmail.com>
2024-01-10 05:11:39 +0530

    new files added

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,28 +1,38 @@
 package testBase;
 
-import java.time.Duration;
-
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
-import org.apache.logging.log4j.LogManager;//log4j
-import org.apache.logging.log4j.Logger;   //log4j
 import org.testng.annotations.Parameters;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
 
 public class BaseClass {
     public WebDriver driver;
     public Logger logger;
+    public Properties p;
 
     @BeforeClass
-    @Parameters ({"os", "browser"})
-    public void setup(String os, String br) {
+    @Parameters({"os", "browser"})
+    public void setup(String os, String br) throws IOException {
+//        loading properties file
+        FileReader file = new FileReader("D:\\Automation Testing\\OpenCartProject\\src\\test\\resources\\config.properties");
+        p = new Properties();
+        p.load(file);
+
+//        loading log4j file
         logger = LogManager.getLogger(this.getClass());
 
-        switch (br.toLowerCase()){
+        switch (br.toLowerCase()) {
             case "chrome":
                 driver = new ChromeDriver();
                 break;
@@ -37,7 +47,7 @@ public class BaseClass {
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        driver.get("http://localhost:8080/opencart/index.php");
+        driver.get(p.getProperty("appURL"));
         driver.manage().window().maximize();
     }
 
@@ -45,6 +55,7 @@ public class BaseClass {
     public void tearDown() {
         driver.quit();
     }
+
     public String randomString() {
         return RandomStringUtils.randomAlphabetic(5);
     }
