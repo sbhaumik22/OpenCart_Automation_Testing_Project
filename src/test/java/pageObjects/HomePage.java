1eae3a4d266142b9131fe958f4ef160a3b760a76
Soumesh Bhaumik <soumeshbhaumik99@gmail.com>
2024-01-10 13:17:57 +0530

    login files added

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class HomePage extends BasePage{
+public class HomePage extends BasePage {
     WebDriver driver;
 
     public HomePage(WebDriver driver) {
@@ -17,11 +17,21 @@ public class HomePage extends BasePage{
     @FindBy(xpath = "//a[normalize-space()='Register']")
     WebElement lnkRegister;
 
+    @FindBy(linkText = "Login")   // Login link added in step5
+    WebElement linkLogin;
+
+
     public void clickMyAccount() {
         lnkMyaccount.click();
     }
 
     public void clickRegister() {
         lnkRegister.click();
+    }
+
+
+    public void clickLogin()    // added in step5
+    {
+        linkLogin.click();
     }
 }
