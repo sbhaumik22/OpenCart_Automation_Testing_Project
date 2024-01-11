1eca37aa9130e8adcea2605595a8a36cee027fa6
Soumesh Bhaumik <soumeshbhaumik99@gmail.com>
2024-01-11 20:04:52 +0530

    groups added

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -4,10 +4,13 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -15,6 +18,7 @@ import org.testng.annotations.Parameters;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -28,7 +32,7 @@ public class BaseClass {
 
     @BeforeClass (groups = {"sanity", "regression", "master"})
     @Parameters({"os", "browser"})
-    public void setup(String os, String br) throws IOException {
+    public void setup(String os, String br) throws IOException{
 //        loading properties file
         FileReader file = new FileReader("D:\\Automation Testing\\OpenCartProject\\src\\test\\resources\\config.properties");
         p = new Properties();
@@ -37,16 +41,47 @@ public class BaseClass {
 //        loading log4j file
         logger = LogManager.getLogger(this.getClass());
 
-        switch (br.toLowerCase()) {
-            case "chrome":
-                driver = new ChromeDriver();
-                break;
-            case "edge":
-                driver = new EdgeDriver();
-                break;
-            default:
-                System.out.println("No matching browser...");
+        if (p.getProperty("execution_env").equalsIgnoreCase("remote")){
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+//            os
+            if (os.equalsIgnoreCase("windows")) {
+                capabilities.setPlatform(Platform.WIN11);
+            }
+            else if (os.equalsIgnoreCase("mac")){
+                capabilities.setPlatform(Platform.MAC);
+            }
+            else {
+                System.out.println("No matching OS");
                 return;
+            }
+//            launching browser remotely
+            switch (br){
+                case "chrome":
+                    capabilities.setBrowserName("chrome");
+                    break;
+                case "edge":
+                    capabilities.setBrowserName("MicrosoftEdge");
+                    break;
+                default:
+                    System.out.println("No matching browser.");
+            }
+
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+        }
+
+//        launching browser locally
+        else if (p.getProperty("execution_env").equalsIgnoreCase("local")) {
+            switch (br.toLowerCase()) {
+                case "chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "edge":
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    System.out.println("No matching browser...");
+                    return;
+            }
         }
 
         driver.manage().deleteAllCookies();
